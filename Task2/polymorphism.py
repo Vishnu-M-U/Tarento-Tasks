c61f9006a4997884bfcb46d0ea0c8a2ef1e60ff8
Vishnu-M-U <vishnumu29@gmail.com>
2023-09-16 11:06:06 +0530

    added python file

---
+++ Task2/polymorphism.py
@@ -0,0 +1,34 @@
+class Shape:
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14159265359 * self.radius * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+
+shapes = [Circle(5), Rectangle(4, 6), Triangle(3, 8)]
+
+
+for shape in shapes:
+    print(f"Shape: {shape.__class__.__name__}")
+    print(f"Area: {shape.area()}\n")
